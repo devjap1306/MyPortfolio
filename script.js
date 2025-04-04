35f01b5825d4c4964b9145f499f5691668f5b9c2
devjap1306 <devjpatel1306@gmail.com>
2025-04-03 21:48:35 -0400

    Update script.js
    last

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function opentab(tabname){
         tabcontent.classList.remove("active-tab");
     }
 
+
+
+    
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
