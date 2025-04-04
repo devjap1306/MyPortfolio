c5fa7ef0a795a31ef760454e590fd514c467d0f6
devjap1306 <devjpatel1306@gmail.com>
2025-04-03 21:50:32 -0400

    space

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ function opentab(tabname){
         tabcontent.classList.remove("active-tab");
     }
 
-
-
-    
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
